43fdd1fc2cbd373f1ebe460e53b488b935c0aee3
Ewan Bauld <ewan.bauld@gmail.com>
2023-04-11 19:56:10 +0100

    Include Material UI Grid

--- src/components/RepoList.tsx
+++ src/components/RepoList.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import IRepo from '../types/repo'
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+
 
 interface RepoListProps {
     repos: Array<IRepo>
 }
 
+const columns: GridColDef[] = [
+    { field: 'name', headerName: 'Repo Name', width: 250 },
+    { field: 'stargazers_count', headerName: 'Stars', width: 30 },
+    { field: 'forks', headerName: 'Forks', width: 30 },
+    {
+      field: 'clone_url',
+      headerName: 'Repo Link',
+      width: 290
+    }
+];
+
 const RepoList: React.FC<RepoListProps> = ({repos}) => {
     return (
         <div className='list-container'>
-            {repos.length > 0 && repos.map((repo) => {
-                return (
-                    <div className='wrapper'>
-                        <div>{repo.name}</div>
-                        <div>{repo.stars}</div>
-                        <div>{repo.forks}</div>
-                    </div>
-                )
-            })}
+            <DataGrid
+                rows={repos}
+                columns={columns}
+                autoHeight={true}
+          />
         </div>
     )
 };
